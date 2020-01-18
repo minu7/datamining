5840dcccea0a74ea377878a9d223c5bfe05045eb
Filippo Minutella <filippominutella@gmail.com>
2020-01-18 18:49:05 +0100

    import documents in acquisition to mongo

--- app/excel_to_mongo/acquisitions.py
+++ app/excel_to_mongo/acquisitions.py
@@ -8,12 +8,12 @@ import xlrd
 from api.db import Acquisition
 from datetime import datetime
 from excel_date_to_datetime import excel_to_datetime
+import pymongo
 # from bson.objectid import ObjectId
 # to run this: docker-compose run flask python app/excel_to_mongo/acquisitions.py
 
 data = xlrd.open_workbook(filename = 'app/data/initialdata.xlsx')
 acquisitions = data.sheets()[0]
-documents = data.sheets()[3]
 
 rows = acquisitions.get_rows()
 
@@ -24,17 +24,23 @@ for i, val in enumerate(rows):
     document = {
         "annuncement_date" : excel_to_datetime(val[0].value),
         "signing_date": excel_to_datetime(val[1].value),
-        "status": val[2].value,
+        "status": val[2].value.lower().strip(),
         "acquiror": {
             "name": val[4].value,
-            "ticker": val[5].value,
-            "state": val[6].value,
+            "ticker": val[5].value.lower().strip(),
+            "state": val[6].value.lower().strip(),
         },
         "target": {
             "name": val[7].value,
-            "ticker": val[8].value,
-            "state": val[9].value
+            "ticker": val[8].value.lower().strip(),
+            "state": val[9].value.lower().strip()
         },
+        "op_id": val[10].value,
         "documents": []
     }
     Acquisition.insert_one(document)
+# ad unique index on op_id
+Acquisition.create_index(
+    [("op_id", pymongo.ASCENDING)],
+    unique=True
+)
---
+++ app/excel_to_mongo/documents.py
@@ -0,0 +1,34 @@
+import sys
+import os
+from os.path import dirname, abspath
+
+sys.path.insert(1, dirname(dirname(abspath(__file__)))) # this is needed for import Acquisition (we need to add the root folder in module search paths)
+
+import xlrd
+from api.db import Acquisition
+from datetime import datetime
+from excel_date_to_datetime import excel_to_datetime
+import re
+
+data = xlrd.open_workbook(filename = 'app/data/initialdata.xlsx')
+documents = data.sheets()[3]
+rows = documents.get_rows()
+
+for i, val in enumerate(rows):
+    if i == 0:
+        continue # discard header
+
+    link = val[0].value
+    date = re.split(',|;', val[1].value.lower())[0].strip()
+    date = datetime.strptime(date, '%m/%d/%y')
+    source = re.split(',|;', val[1].value.lower())[1].strip().replace("\"", "")
+    type = val[2].value.lower().strip()
+    op_id = int(val[3].value)
+
+    document = {
+        "link": link,
+        "date": date,
+        "source": source,
+        "type": type
+    }
+    Acquisition.update_one({ "op_id": op_id }, { '$push': {'documents': document} })
